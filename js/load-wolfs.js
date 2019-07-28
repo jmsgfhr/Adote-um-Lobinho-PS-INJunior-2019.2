ee52bc03bf46a7a0b993335a8216aef966e71a2f
Jonathan Meirelles <jonathanms30@gmail.com>
2019-07-28 15:45:53 -0300

    Todos os lobos nao adotados carregam na pagina wolf-list

--- js/load-wolfs.js
+++ js/load-wolfs.js
@@ -1,3 +1,74 @@
 function load() {
-    
+    var path = window.location.pathname;
+    var page = path.split("/").pop();
+
+    let list = document.querySelector(".list .container")
+    const myHeaders = {
+        "Content-Type": "application/json"
+    }
+
+    const fetchingConfig = {
+        method: 'GET',
+        headers: myHeaders
+    }
+
+    let wolfs;
+
+    fetch('https://lobinhos-api.herokuapp.com/wolves', fetchingConfig)
+        .then((request) => {
+            return request.json();
+        })
+        .then((responseAsJson) => {
+            wolfs = responseAsJson;
+        }).then(() => {
+            if (page === 'index.html') {
+                
+            }
+            else {
+                for (let i = 0; i < wolfs.length; i++) {
+                    if (wolfs[i].adoption == null) {
+                        let newWolf = document.createElement('div');
+                        newWolf.classList.add("wolf-list");
+                        let newWolfPhoto = document.createElement('img');
+                        if (i % 2 == 0) {
+                            newWolfPhoto.classList.add('wolf-left');
+                        }
+                        else {
+                            newWolfPhoto.classList.add('wolf-right');
+                        }
+                        newWolfPhoto.src = wolfs[i].photo;
+                        let newWolfList = document.createElement('div');
+                        if (i % 2 == 0) {
+                            newWolfList.classList.add("wolf-list-left");
+                        }
+                        else {
+                            newWolfList.classList.add("wolf-list-right");
+                        }
+                        let newWolfButton = document.createElement('button');
+                        if (i % 2 == 0) {
+                            newWolfButton.classList.add("list-left-wolf");
+                        }
+                        else {
+                            newWolfButton.classList.add("list-right-wolf");
+                        }
+                        newWolfButton.innerText = "Adotar";
+                        let newWolfName = document.createElement('h3');
+                        newWolfName.classList.add('wolf-name');
+                        newWolfName.innerText = wolfs[i].name;
+                        let newWolfAge = document.createElement('p');
+                        newWolfAge.classList.add('wolf-age');
+                        let newWolfDescription = document.createElement('p');
+                        newWolfDescription.innerText = wolfs[i].description;
+                        newWolfAge.innerText = `Idade: ${wolfs[i].age} anos`;
+                        newWolfList.appendChild(newWolfButton);
+                        newWolfList.appendChild(newWolfName);
+                        newWolfList.appendChild(newWolfAge);
+                        newWolfList.appendChild(newWolfDescription);
+                        newWolf.appendChild(newWolfPhoto);
+                        newWolf.appendChild(newWolfList);
+                        list.appendChild(newWolf);
+                    }
+                }
+            }
+        })
 }
